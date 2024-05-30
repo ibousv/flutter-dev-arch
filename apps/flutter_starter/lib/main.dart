eb934731ee4f7ba801d030de53a9c0020e2eb4cd
ibousv <devilsog2.0@gmail.com>
2024-05-30 10:14:50 +0000

    update

--- apps/flutter_starter/lib/main.dart
+++ apps/flutter_starter/lib/main.dart
@@ -1,22 +1,42 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: camel_case_types
 
+import 'package:flutter/material.dart';
+import 'package:flutter_starter/splash_screen/splash_screen_page.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:get/route_manager.dart';
+import 'package:flutter_starter/onboarding/onboarding_page.dart';
 
 void main(List<String> args) {
-  runApp(const MyWidget());
+  runApp(const MainApp());
 }
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
-  
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<MainApp> createState() => _MyWidgetState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _MyWidgetState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-     
+    return GetMaterialApp(
+      title: 'Gestion de dépenses',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: true,
+     initialRoute: '/onboarding',
+     getPages: [
+       GetPage(
+         name: '/onboarding',
+         page: () => OnboardingView(),
+       ),
+      
+     ],
     );
   }
 }
+
