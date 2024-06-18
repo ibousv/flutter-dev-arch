9d3839da9668e886ce5cc0c00d30435f8a817af1
ibousv <devilsog2.0@gmail.com>
2024-06-18 08:04:05 +0000

    fix

--- apps/flutter_starter/lib/main.dart
+++ apps/flutter_starter/lib/main.dart
@@ -27,14 +27,16 @@ class HomeState extends State<App> {
         });
   }
 }
+
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
 
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+  
 }
 
 class _MyHomePageState extends State<MyHomePage> {
